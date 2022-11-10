83035bc832d1fb4b9eff070987defa1da977b1a2
meilyanaanisa <meilyanaanisa@gmail.com>
2022-11-10 22:35:35 +0700

    memindahkan file ke folder

---
+++ src/components/DetailCourse/DetailCourse.jsx
@@ -0,0 +1,33 @@
+import {useParams} from "react-router-dom"
+import courseData from '../Course/CourseData.json'
+import './DetailCourse.css'
+
+const DetailCourse = () =>  {
+
+    const { id } = useParams();
+    console.log(id);
+
+    return (
+        <section className="detailcourse-page">
+            {courseData
+                .filter(( el ) => el.id ===+id)
+                .map(( el ) => {
+                    return (
+                        <div className="detailcourse-description" key={el.id}>
+                            <div className="detail-judul">
+                            <h2> {el.judul} </h2>
+                            </div>
+                            <div className="detail-gambar">
+                            <img src={el.gambar} alt="" />
+                            </div>
+                            <div className="detail-deskripsi">
+                            <p> {el.descriptionfull}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+        </section>
+    )
+}
+
+export default DetailCourse
