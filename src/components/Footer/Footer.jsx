6d1a4beb297265949a9b721aa843b3776b0f00b5
meilyanaanisa <meilyanaanisa@gmail.com>
2022-11-10 15:57:23 +0700

    membuat file footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import  ICInstagram  from '../assetsimg/instagram.png'
+import  ICLinkedin  from '../assetsimg/linkedin.png'
+import  ICGmail  from '../assetsimg/gmail.png'
+import './Footer.css'
+
+
+function footer() {
+  return (
+    <footer className='footer'>
+      <div className="footer-nav">
+        <h2>Explore</h2>
+        <ul>
+          <Link to='/' style={{textDecoration: 'none'}}>
+          <li className='li'>Home</li>
+          </Link>
+          <Link to='about' style={{textDecoration: 'none'}}>
+          <li>About</li>
+          </Link>
+          <Link to='course' style={{textDecoration: 'none'}}>
+          <li>Course</li>
+          </Link>
+          <Link to='blog' style={{textDecoration: 'none'}}>
+          <li>Blog</li>
+          </Link>
+        </ul>
+      </div>
+      <div className="footer-media">
+        <div className="footer-media-description">
+        <h2>Closer with me. Do you have <br /> some feedback or suggestions?</h2>
+        <h3>You can also reach me on Social Media</h3>
+        </div> 
+
+        <div className="footer-media-social" style={{display:'flex'}}>
+          <a href="https://www.instagram.com/meilyanaanisaa">
+          <img src={ICInstagram}/>
+          </a>
+
+          <a href="https://www.linkedin.com/in/meilyana-anisa-mawarti">
+          <img src={ICLinkedin} />
+          </a>
+
+          <a href="https://meilyanaanisa@gmail.com">
+          <img src={ICGmail} />
+          </a>
+        </div>
+      </div> 
+
+    </footer>
+  )
+}
+
+export default footer
