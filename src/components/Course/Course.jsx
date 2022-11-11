3d3db5f37ee5efc1af2bf241ea94720047da2972
meilyanaanisa <meilyanaanisa@gmail.com>
2022-11-11 10:50:41 +0700

    mengganti path json

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Course.css'
 import { useNavigate } from 'react-router-dom'
-import coursedata from './CourseData.json'
+import coursedata from '../../CourseData.json'
 
 function Course() {
 
