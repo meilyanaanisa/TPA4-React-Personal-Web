c89514ed2417a938a1aa82ced92e905bf83b82ba
meilyanaanisa <meilyanaanisa@gmail.com>
2022-11-11 13:16:54 +0700

    memberikan className

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -15,7 +15,9 @@ function Blog() {
           <p>
 We take a look at using Netlify to build, deploy and host modern websites. Netlify makes deploying websites a breeze with awesome features like git integration, automatic deployment, builds on the server, easy https, custom domains, contact forms and much more. We deploy four projects; a static site on our local machine, a static site from a GitHub repo, a Vue CLI app, and a React CLI app.
 </p>
+          <div className="blog-card-button">
           <button><a style= {{textDecoration: 'none'}} href="https://www.youtube.com/watch?v=eOKLccIYcBc"> Watch Video </a></button>
+          </div>
           </div>
         </div>
 
@@ -25,7 +27,10 @@ We take a look at using Netlify to build, deploy and host modern websites. Netli
           <div className="blog-card-description">
           <h2>ReactJS Basics - #1 What is React?</h2>
           <p> Academind is your source for online education in the areas of web development, frontend web development, backend web development, programming, coding and data science! No matter if you are looking for a tutorial, a course, a crash course, an introduction, an online tutorial or any related video, we try our best to offer you the content you are looking for. </p>
+          
+          <div className="blog-card-button">
           <button> <a style={{textDecoration: 'none'}} href="https://www.youtube.com/watch?v=JPT3bFIwJYA"> Watch Video </a></button>
+          </div>
           </div>
         </div>
 
@@ -35,7 +40,10 @@ We take a look at using Netlify to build, deploy and host modern websites. Netli
           <div className="blog-card-description">
           <h2>HTML & CSS for Beginners </h2>
           <p> In this free course, youll learn how to code with modern HTML and CSS, the main building blocks of any website. If you want to become a successful web developer, this is where you need to start! At the end of more than eight hours of learning where we cover everything from basic HTML elements to working with Flexbox and CSS Grid, youll test your new skills by building a complete web page.</p>
+          
+          <div className="blog-card-button">
           <button> <a style={{textDecoration: 'none'}} href="https://www.youtube.com/watch?v=iG2jotQo9NI"> Watch Video </a></button>
+          </div>
           </div>
         </div>
 
@@ -44,7 +52,9 @@ We take a look at using Netlify to build, deploy and host modern websites. Netli
           <div className="blog-card-description">
           <h2>What is Json </h2>
           <p> In this video I will explain you, what JSON (JavaScript Object Notation) is and what it is used for. We will also take a closer look at the syntax of JSON objects and JSON arrays.</p>
+          <div className="blog-card-button">
           <button> <a style={{textDecoration: 'none'}} href="https://www.youtube.com/watch?v=0IoG-mSvWSo"> Watch Video </a></button>
+          </div>
           </div>
         </div>
     </section>
